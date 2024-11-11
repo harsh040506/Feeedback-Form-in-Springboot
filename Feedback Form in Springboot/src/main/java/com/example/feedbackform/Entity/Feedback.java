3c072ea4930d839218725af0015a7e6553f450ab
Harsh Chhajer <harshprashantchhajer@gmail.com>
2024-11-11 13:59:01 +0530

    Update Feedback.java

--- Feedback Form in Springboot/src/main/java/com/example/feedbackform/Entity/Feedback.java
+++ Feedback Form in Springboot/src/main/java/com/example/feedbackform/Entity/Feedback.java
@@ -9,26 +9,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "Users") // Specify your desired table name here
 public class Feedback {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private Integer form_id;
     private String question1;
     private String question2;
     private String question3;
     private String question4;
-
     private String question5;
     private String question6;
     private String question7;
-
     private String question8;
     private String question9;
     private String question10;
     private String question11;
-
     private String question12;
     private String question13;
     private String question14;
